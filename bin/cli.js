5566879cb4f669642112606f8e07f817ab9d56e4
Yuto Kojima <Yuto Kojima>
2016-06-27 20:58:06 +0900

    add argument options logic

--- bin/cli.js
+++ bin/cli.js
@@ -1,24 +1,35 @@
 let path = require('path');
     yargs = require('yargs'),
     mdBrowserPreview = require('../lib/index'),
-    util = require('../lib/util');
+    util = require('../lib/util'),
+    argv = require('../lib/argv');
 
-let argv = yargs.argv,
-    input = argv._[0];
+let htdocs = path.join(__dirname, '../tmp'),
+    inputFileName = argv._[0],
+    inputFilePath =path.join(process.cwd(), inputFileName),
+    outputFileName = util.extReplace(path.basename(inputFilePath)),
+    outputFilePath = path.join(htdocs, outputFileName);
 
 let options = {
-    target: {
-        md: path.join(process.cwd(), input),
-        html: util.extReplace(path.basename(input)),
-        dir: 'tmp'
+    path: {
+        md: inputFilePath,
+        htdocs: htdocs
     },
     marked: {
-        gfm: true,
+        gfm: argv.gfm,
         fileRename: util.extReplace
     },
     wrap: {
-        title: 'example'
+        title: argv.title
     },
+    browserSync: {
+        startPath: '/' + outputFileName,
+        server: {
+            baseDir: htdocs
+        },
+        port: argv.port,
+        browser: argv.browser
+    }
 };
-
+console.log(options);
 mdBrowserPreview(options);
--- target.md
+++ target.md
@@ -12,4 +12,5 @@
 1. first
 2. second
 3. third
-4. fourth
+
+`source code`
